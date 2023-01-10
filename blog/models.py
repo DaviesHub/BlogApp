62aad69e434abe6946d52240843f90e76ac5c05f
DaviesHub <davieschuks.a@gmail.com>
2023-01-10 21:49:31 +0200

    Add post commente

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,12 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-pub_date',)
+
+# ENABLING COMMENTS
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
