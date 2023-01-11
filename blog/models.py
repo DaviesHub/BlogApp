863f8eb91ae81530605743790c7c9ebca08cbf0c
DaviesHub <davieschuks.a@gmail.com>
2023-01-11 20:34:25 +0200

    Add category model

--- blog/models.py
+++ blog/models.py
@@ -21,3 +21,12 @@ class Comment(models.Model):
     email = models.EmailField()
     body = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
+
+# ENABLING CATEGORIZATION
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('title',)
