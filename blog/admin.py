094a1ecc950f58ac3f31423de94f6ed03c3682ed
DaviesHub <davieschuks.a@gmail.com>
2023-01-12 13:03:57 +0200

    Add comments in admin view

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
 
-from .models import Post, Category
+from .models import Post, Category, Comment
+
+class CommentItemInLine(admin.TabularInline):
+    model = Comment
+    raw_id_fields = ['post']
 
 class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'intro', 'body']
     list_display = ['title', 'slug', 'category', 'pub_date']
     list_filter = ['category', 'pub_date']
+    inlines = [CommentItemInLine]
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['title']
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'post', 'pub_date']  
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
