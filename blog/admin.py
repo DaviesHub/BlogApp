24ec2ef3ff849f290133e33e91c336ae5a6b07ce
DaviesHub <davieschuks.a@gmail.com>
2023-01-12 10:42:03 +0200

    Add search bar and list filter in admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Post, Category
 
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'intro', 'body']
+    list_display = ['title', 'slug', 'category', 'pub_date']
+    list_filter = ['category', 'pub_date']
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
